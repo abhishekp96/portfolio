290133da6f82aee3b1cabf704aa753c58bc55e3b
abhishekp96 <79747596+abhishekp96@users.noreply.github.com>
2022-06-23 20:02:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
+
+function navigation() {
+  var x = document.getElementById("navbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
